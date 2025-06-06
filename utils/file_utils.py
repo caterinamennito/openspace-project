efa34e7f8d5260181556add46d88b1dbf260c09b
topmanu_col <manusp33@hotmail.com>
2025-06-06 14:01:23 +0200

    changed file utils

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,7 +1,7 @@
 #the function opens and reads a file of colleagues
 def file_utils(file_path):
-    with open(file_path, 'r') as my_file:
+    with open(file_path, newline='') as my_file:
             #put list of colleagues in a list
-            list_colleagues = my_file.read()
-            print(list_colleagues)
+            colleagues = my_file.read()
+            list_colleagues = colleagues.splitlines()
     return list_colleagues
